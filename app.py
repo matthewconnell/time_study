95e1276f3885911452ef08e7c8899003a111d292
Matt <matthew.at.connell@gmail.com>
2020-09-22 13:22:34 -0700

    Created new column in db

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    time_diff = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return "<Task %r>" % self.id
