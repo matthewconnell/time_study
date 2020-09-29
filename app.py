9e898358877172de1cc5c17f38400d4996f31efb
Matt <matthew.at.connell@gmail.com>
2020-09-28 18:07:03 -0700

    Removed end button from 'Action' column
    Made the end button at the top functional
    
    Change entry form so most recent name will stay in the form.

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def index():
 
     else:
         tasks = Task.query.order_by(Task.date_created).all()
-        return render_template('index.html', tasks = tasks)
+        return render_template('index.html', 
+                                tasks = tasks)
 
 @app.route('/delete/<int:id>')
 def delete(id):
