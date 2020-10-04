504ccff74cbc1fb673b010afb5dcf51af7d17c16
Matt <matthew.at.connell@gmail.com>
2020-10-03 23:45:50 -0700

    Changed name of db to 'time_study'

--- app.py
+++ app.py
@@ -12,16 +12,18 @@ import random
 from vega_datasets import data
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///time_study.db'
 db = SQLAlchemy(app)
 
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    occurence = db.Column(db.Integer, default=0)
+    operator = db.Column(db.String(200), nullable=False)
+    observation = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     time_diff = db.Column(db.Integer, default=0)
+    element = db.Column(db.Integer, default=0)
+    
 
     def __repr__(self):
         return "<Task %r>" % self.id
